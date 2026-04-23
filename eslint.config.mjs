8b3774708f57b3e848a93ae963c4600afa57af91
Bhivanshu45 <bhivanshulawaniya6@gmail.com>
2026-04-24 03:56:05 +0530

    remove specific linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,9 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "prefer-const": "off",
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-html-link-for-pages": "off",
+      "react-hooks/rules-of-hooks": "off",
     },
   },
 ];
